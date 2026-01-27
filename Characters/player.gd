7ad4294aa0b31739a312d394c874a1a465db3cd7
clpanini <chloelainedpangilinan@gmail.com>
2026-01-27 13:58:14 +0800

    Added River Movement

--- Characters/player.gd
+++ Characters/player.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var river_push_speed : float = 100
 @export var starting_direction : Vector2 = Vector2(0,1)
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
-# Using your specific node path
 @onready var tile_map: TileMapLayer = $"../TileMap/Base"
+@onready var river_map: TileMapLayer = $"../TileMap/River"
 
 var last_input_direction : Vector2 = Vector2.ZERO
 var sliding = false 
@@ -19,13 +20,18 @@ func _ready():
 func _physics_process(_delta):
 	sliding = is_on_ice()
 	
+	var river_dir = get_river_dir()
+	var river_sliding = river_dir != Vector2.ZERO
+	var force_slide = sliding or river_sliding
+	
 	var input_direction = Vector2.ZERO
 	
-	# If we are on ice AND we haven't hit a wall yet
-	if sliding and last_input_direction != Vector2.ZERO:
+	if river_sliding:
+		input_direction = river_dir
+		last_input_direction = river_dir
+	elif sliding and last_input_direction != Vector2.ZERO:
 		input_direction = last_input_direction
 	else:
-		# Normal walking logic
 		var x = Input.get_axis("left", "right")
 		var y = Input.get_axis("up", "down")
 	
@@ -37,25 +43,23 @@ func _physics_process(_delta):
 		if input_direction != Vector2.ZERO:
 			last_input_direction = input_direction.normalized()
 	
-	# Face the direction of travel
-	update_animation_parameters(last_input_direction if sliding else input_direction)
+	update_animation_parameters(last_input_direction if force_slide else input_direction)
 	
-	velocity = input_direction * move_speed
+	if river_sliding:
+		velocity = river_dir * river_push_speed
+	else:
+		velocity = input_direction * move_speed
 	
-	# Move the player and check for collisions
 	move_and_slide()
 	
-	# --- FIX FOR FACING THE TREE ---
-	# If we are sliding but our 'real' movement stopped (we hit the tree)
-	if sliding and get_slide_collision_count() > 0:
-		last_input_direction = Vector2.ZERO # This stops the 'force' into the tree
+	if force_slide and get_slide_collision_count() > 0:
+		last_input_direction = Vector2.ZERO
 	
-	pick_new_state()
+	pick_new_state(force_slide)
 	
 func is_on_ice() -> bool:
 	if not tile_map: return false
 	
-	# Adjust (0, 4) if your player needs to be deeper into the tile to slide
 	var current_tile = tile_map.local_to_map(global_position + Vector2(0, 4))
 	var data = tile_map.get_cell_tile_data(current_tile)
 	
@@ -63,14 +67,27 @@ func is_on_ice() -> bool:
 		return data.get_custom_data("is_ice")
 	return false
 
+func get_river_dir() -> Vector2:
+	if not river_map:
+		return Vector2.ZERO
+	
+	var current_tile = river_map.local_to_map(global_position + Vector2(0, 4))
+	var data = river_map.get_cell_tile_data(current_tile)
+	
+	if data:
+		if data.get_custom_data("river_down") == true:
+			return Vector2(0, 1)
+		if data.get_custom_data("river_right") == true:
+			return Vector2(1, 0)
+	return Vector2.ZERO
+
 func update_animation_parameters(move_input : Vector2):
 	if(move_input != Vector2.ZERO):
 		animation_tree.set("parameters/Walk/blend_position", move_input)
 		animation_tree.set("parameters/Idle/blend_position", move_input)
 		
-func pick_new_state():
-	# Only walk if we have actual velocity and aren't sliding
-	if velocity != Vector2.ZERO and not sliding:
+func pick_new_state(force_slide: bool):
+	if velocity != Vector2.ZERO and not force_slide:
 		state_machine.travel("Walk")
 	else:
 		state_machine.travel("Idle")
